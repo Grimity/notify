0b9d01fa1fc83923eff0e87efd13058bd218f495
leemhoon00 <leemhoon000@gmail.com>
2025-01-18 23:38:24 +0900

    remove dynamodb

--- src/db/types.ts
+++ src/db/types.ts
@@ -1,63 +1,74 @@
-import type { ColumnType } from 'kysely';
+import type { ColumnType } from "kysely";
 export type Generated<T> = T extends ColumnType<infer S, infer I, infer U>
   ? ColumnType<S, I | undefined, U>
   : ColumnType<T, T | undefined, T>;
 export type Timestamp = ColumnType<Date, Date | string, Date | string>;
 
 export type Feed = {
-  id: string;
-  authorId: string;
-  title: string;
-  cards: string[];
-  isAI: Generated<boolean>;
-  createdAt: Generated<Timestamp>;
-  viewCount: Generated<number>;
-  likeCount: Generated<number>;
-  content: Generated<string>;
+    id: string;
+    authorId: string;
+    title: string;
+    cards: string[];
+    isAI: Generated<boolean>;
+    createdAt: Generated<Timestamp>;
+    viewCount: Generated<number>;
+    likeCount: Generated<number>;
+    content: Generated<string>;
 };
 export type FeedComment = {
-  id: string;
-  feedId: string;
-  writerId: string;
-  parentId: string | null;
-  content: string;
-  createdAt: Generated<Timestamp>;
+    id: string;
+    feedId: string;
+    writerId: string;
+    parentId: string | null;
+    content: string;
+    createdAt: Generated<Timestamp>;
 };
 export type Follow = {
-  followerId: string;
-  followingId: string;
+    followerId: string;
+    followingId: string;
 };
 export type Like = {
-  userId: string;
-  feedId: string;
-  createdAt: Generated<Timestamp>;
+    userId: string;
+    feedId: string;
+    createdAt: Generated<Timestamp>;
+};
+export type Notification = {
+    userId: string;
+    createdAt: Generated<Timestamp>;
+    id: string;
+    type: string;
+    actorId: string;
+    actorName: string;
+    feedId: string | null;
+    isRead: Generated<boolean>;
 };
 export type Tag = {
-  feedId: string;
-  tagName: string;
+    feedId: string;
+    tagName: string;
 };
 export type User = {
-  id: string;
-  provider: string;
-  providerId: string;
-  email: string;
-  name: string;
-  image: string | null;
-  description: Generated<string>;
-  links: string[];
-  createdAt: Generated<Timestamp>;
+    id: string;
+    provider: string;
+    providerId: string;
+    email: string;
+    name: string;
+    image: string | null;
+    description: Generated<string>;
+    links: string[];
+    createdAt: Generated<Timestamp>;
 };
 export type View = {
-  userId: string;
-  feedId: string;
-  createdAt: Generated<Timestamp>;
+    userId: string;
+    feedId: string;
+    createdAt: Generated<Timestamp>;
 };
 export type DB = {
-  Feed: Feed;
-  FeedComment: FeedComment;
-  Follow: Follow;
-  Like: Like;
-  Tag: Tag;
-  User: User;
-  View: View;
+    Feed: Feed;
+    FeedComment: FeedComment;
+    Follow: Follow;
+    Like: Like;
+    Notification: Notification;
+    Tag: Tag;
+    User: User;
+    View: View;
 };
